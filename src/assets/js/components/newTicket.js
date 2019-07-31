273b590df8dd7980b03093eaba207565951369ab
Brandon Cranmore <brandon.cranmore@papamurphys.com>
2019-07-31 14:31:40 -0700

    updated input fields to correct data type

--- src/assets/js/components/newTicket.js
+++ src/assets/js/components/newTicket.js
@@ -33,24 +33,24 @@ loadGreeting(greeting);
 function generateOutput(){
     let fname = document.getElementById('fname').value;
     let lname = document.getElementById('lname').value;
-    console.log(fname);
-    console.log(lname);
+    //console.log(fname);
+    //console.log(lname);
     let state = document.getElementById('state').options[document.getElementById('state').selectedIndex].value;
-    console.log(state);
+    //console.log(state);
     let site_id = document.getElementById('site_id').value;
-    console.log(site_id);
+    //console.log(site_id);
     let role = document.getElementById('role').options[document.getElementById('role').selectedIndex].value;
-    console.log(role);
+    //console.log(role);
     let telephone = document.getElementById('telephone').value;
-    console.log(telephone);
+    //console.log(telephone);
     let email = document.getElementById('email').value;
-    console.log(email);
+    //console.log(email);
     let issue = document.getElementById('issue').value;
-    console.log(issue);
+    //console.log(issue);
     let troubleshooting = document.getElementById('troubleshooting').value;
-    console.log(troubleshooting);
+    //console.log(troubleshooting);
     let comments = document.getElementById('comment').value;
-    console.log(comments);
+    //console.log(comments);
 
 };
 
