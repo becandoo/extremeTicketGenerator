fd80230d5ab58a04d4ff5ab2337b46696c4052d7
Brandon Cranmore <bec@Brandons-MacBook-Pro.local>
2019-08-08 08:52:53 -0700

    added test data and modified function names

--- src/assets/js/components/newTicket.js
+++ src/assets/js/components/newTicket.js
@@ -12,7 +12,7 @@ var ticket = {
     troubleshooting_steps_completed: "",
     comments: "",
     store_number : function(){
-        return this.state + this.site_id;
+        return this.state.toUpperCase() + this.site_id;
     },
     name : function(){
         return this.fname + " " + this.lname;
@@ -30,7 +30,7 @@ function loadGreeting(grt) {
 
 loadGreeting(greeting);
 
-function generateOutput(){
+function buildTicketObj(){
     let fname = document.getElementById('fname').value;
     ticket.fname = fname;
     let lname = document.getElementById('lname').value;
@@ -45,22 +45,40 @@ function generateOutput(){
     let telephone = document.getElementById('telephone').value;
     ticket.telephone = telephone;
     let email = document.getElementById('email').value;
-    ticket.email = email;
+    ticket.email_address = email;
     let issue = document.getElementById('issue').value;
     ticket.issue = issue;
     let troubleshooting = document.getElementById('troubleshooting').value;
     ticket.troubleshooting_steps_completed = troubleshooting;
     let comments = document.getElementById('comment').value;
     ticket.comments = comments;
-    insertOutput();
+
+    let outputString = 'Name: ' + ticket.name() + '\n' + 
+                       'Store Number: ' + ticket.store_number() + '\n' +
+                       'Telephone: ' + ticket.telephone + '\n' +
+                       'Role: ' + ticket.role + '\n' +
+                       'Email: ' + ticket.email_address + '\n' +
+                       'Issue: ' + ticket.issue;
+
+    generateOutput(outputString);
+}
+function generateOutput(str){
+    let outputBox = document.getElementById('output');
+    outputBox.value = str;
 };
 
 document.getElementById('generateOutput').onclick =  function(){
-    generateOutput();
+    buildTicketObj();
 };
 
-function insertOutput(){
-    console.log(ticket.name());
-    let outputBox = document.getElementById('output');
-    
+
+function loadTestData() {
+    document.getElementById('fname').value = 'Johnny';
+    document.getElementById('lname').value = 'Smithy';
+    document.getElementById('site_id').value = '019';
+    document.getElementById('telephone').value = '555-555-5555';
+    document.getElementById('email').value = 'test@gmail.com';
+    document.getElementById('issue').value = 'FCR Bin Sync Error';
 };
+
+loadTestData();
